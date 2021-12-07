349d1d359e1e3aca0791ee7c10d0c15c20976836
baurine <2008.hbl@gmail.com>
2021-12-07 23:26:51 +0800

    workaround

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import './style.css'
 // Below line doesn't work with esbuild-plugin-postcss2
 // import 'bulma/css/bulma.css'
 
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
-// import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/light'
+// import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/light'
 import sql from 'react-syntax-highlighter/dist/esm/languages/hljs/sql'
 import darkTheme from 'react-syntax-highlighter/dist/esm/styles/hljs/atom-one-dark'
 
